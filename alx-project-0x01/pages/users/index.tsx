0db329572b48bc133822bce96e9b0be301723281
Ilechukwu Francis C <ilechukwufran6x@gmail.com>
2025-07-20 22:45:46 +0100

    React Architect: Mastering Advanced Structures in Next.js

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,36 @@
+import UserCard from "@/components/common/UserCard";
 import Header from "@/components/layout/Header";
+import { UserProps } from "@/interfaces";
+
+interface UsersPageProps {
+  posts: UserProps[];
+}
+
+const Users: React.FC<UsersPageProps> = ({ posts }) => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-semibold mb-4">Users</h1>
+        <div className="grid grid-cols-3 gap-2">
+          {posts.map((user: UserProps) => (
+            <UserCard key={user.id} {...user} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
+
+export default Users;
